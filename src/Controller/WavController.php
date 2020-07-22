06f96230b202a00be9959bbe1a4f6156c8f9d15c
TimoArdi <t.ardilouze@gmail.com>
2020-07-22 15:45:05 +0200

    Add composer boostrap jquery

---
+++ src/Controller/WavController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class WavController extends AbstractController
+{
+    /**
+     * @Route("/index", name="wav_index")
+     */
+
+    public function index() : Response
+    {
+         return $this->render('wav/index.html.twig', [
+             'website' => 'Wav Factory',
+    ]);
+    }
+}
